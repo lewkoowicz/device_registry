bf52ad3707b559659f7586b6855ec95d16806d53
lewkoowicz <kajlew1@gmail.com>
2024-10-25 19:23:50 +0200

    Add device-user associations and validations in User and Device models

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,18 @@
 class Device < ApplicationRecord
+  has_many :device_assignments
+  has_many :users, through: :device_assignments
+
+  validates :serial_number, presence: true, uniqueness: true
+
+  def current_assignment
+    device_assignments.active.first
+  end
+
+  def assigned_to?(user)
+    device_assignments.active.exists?(user: user)
+  end
+
+  def ever_assigned_to?(user)
+    device_assignments.exists?(user: user)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_many :device_assignments
+  has_many :devices, through: :device_assignments
   has_many :api_keys, as: :bearer
   has_secure_password
+
+  def active_devices
+    devices.merge(DeviceAssignment.active)
+  end
 end
