3ea4a4388a7e8713cf36a5b127a4a50d16e3257c
lewkoowicz <kajlew1@gmail.com>
2024-10-25 19:27:33 +0200

    Implement return device logic

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -1,11 +1,33 @@
-# frozen_string_literal: true
-
 class ReturnDeviceFromUser
-  def initialize
-    # TODO
+  def initialize(user:, serial_number:, from_user:)
+    @user = user
+    @serial_number = serial_number
+    @from_user = from_user
   end
 
   def call
-    # TODO
+    validate_return!
+
+    assignment = device.current_assignment
+    assignment.update!(
+      status: :returned,
+      returned_at: Time.current
+    )
+  end
+
+  private
+
+  def device
+    @device ||= Device.find_by!(serial_number: @serial_number)
+  end
+
+  def validate_return!
+    unless @user.id == @from_user
+      raise ReturnError::Unauthorized, "Only the assigned user can return a device"
+    end
+
+    unless device.assigned_to?(@user)
+      raise ReturnError::NotAssigned, "Device is not assigned to this user"
+    end
   end
 end
